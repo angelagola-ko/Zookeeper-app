5ee8baff9ca8fb38c8968ee2f6ea1d50eb462347
Angela Gola <angelagola.ko@gmail.com>
2022-06-07 23:08:06 -0400

    POST formatted and working

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express=require('express');
+const fs = require('fs');
+const path = require('path');
+const { features } = require('process');
 
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
@@ -52,15 +59,30 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    const animal = body;
+    // pushes new animal to animal arrau
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finshed code to post route for response.
+    //return body;
+    return animal;
+}
+
 app.get('/api/animals', (req,res) => {
-   let results = animals;
+    let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);//return single animal
     }
     console.log(req.query);
     res.json(results);
-   res.send('Hjnjknjo');
- res.json(animals);
+    res.send('Hjnjknjo');
+    res.json(animals);
 });
 
 app.get('/api/animals/:id', (req,res) => {
@@ -71,6 +93,43 @@ app.get('/api/animals/:id', (req,res) => {
         res.send(404);
     }
 });
+
+app.post('/api/animals', (req,res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted');
+    } else {
+        //add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        //req.body is where our incoming content will be
+        console.log(req.body);
+    
+        res.json(req.body);//send data back to client
+        //access data on server side and do something with it. 
+        //package it up as an object and send it to server
+    }
+
+
+});
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if (!animal.diet || animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 
 
 const { animals } = require('./data/animals.json');
